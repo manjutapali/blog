e707bb9bf32fb991340d8d3c27b5d3088ba3380f
manjutapali <tmanjunath04@gmail.com>
2018-02-02 07:22:39 +0530

    Adding routes with parameters, working with query builder

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,18 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('tasks', function () {
 
 	$tasks = DB::table('tasks')->get();
 
     return view('welcome', compact('tasks'));
+});
+
+Route::get('tasks/{task}', function($id) {
+
+	$task = DB::table('tasks')->find($id);
+
+	return view('tasks.show', compact('task'));
 });
 
 Route::get('about', function() {
