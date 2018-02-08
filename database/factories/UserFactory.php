3dc1ba63d2c821446ccbfa1f0e49d036ecb8cf9e
manjutapali <tmanjunath04@gmail.com>
2018-02-08 23:26:42 +0530

    Writing unit test functions

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,15 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\posts::class, function(Faker $faker) {
+
+	return [
+		'user_id' => function() {
+			return factory(App\User::class)->create()->id;
+		},
+		'title' => $faker->sentence,
+		'content' => $faker->paragraph
+	];
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,9 +4,13 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\posts;
 
 class ExampleTest extends TestCase
 {
+
+	use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -14,6 +18,28 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $first = factory(posts::class)->create();
+
+        $second = factory(posts::class)->create([
+        	'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+
+        $posts = posts::archives();
+
+        //dd($posts);
+
+        $this->assertEquals([
+        	[
+        		"year" => $first->created_at->year,
+    			"month" => $first->created_at->format('F'),
+    			"count(*)" => 1
+        	],
+        	[
+        		"year" => $second->created_at->year,
+    			"month" => $second->created_at->format('F'),
+    			"count(*)" => 1
+        	]
+        ], $posts);
     }
+
 }
