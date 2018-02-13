9f491fd1d8a93af71c56114d121b2679d191ab91
manjutapali <tmanjunath04@gmail.com>
2018-02-13 12:38:43 +0530

    Service containers and service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Billing\Stripe;
 
 class AppServiceProvider extends ServiceProvider
 {
+     /*Feteches the servie provide on requested. But, here boot method is 
+     should be loaded in every page */
+    // protected $defer = true;
+
     /**
      * Bootstrap any application services.
      *
@@ -26,6 +31,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->Singleton(Stripe::class, function($app){
+            return new Stripe(config('services.stripe.secret'));
+        });
     }
 }
